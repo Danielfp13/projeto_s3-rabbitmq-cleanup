39c3aea85b74bc29baa4b15bd4ba0d02b6db77ba
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2024-11-25 16:30:45 -0300

    Criando entidade user.

---
+++ src/main/java/com/daniel/s3/entitites/User.java
@@ -0,0 +1,27 @@
+package com.daniel.s3.entitites;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String imageLink;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
